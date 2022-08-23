08813f7333b55339508783be0c82f648c4070dee
Ross Beamish <frossbeamish@users.noreply.github.com>
2022-08-23 14:22:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,9 @@ module.exports = {
     // http://conqueringthecommandline.com/book/ack_ag
     'ack',
     'ag',
-    'ant',
-    // https://github.com/wting/autojump
-    'autojump',
+    // 'ant',
     // alternative to `cat`: https://github.com/sharkdp/bat
     'bat',
-    'bower',
     // Install GNU core utilities (those that come with macOS are outdated)
     // Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
     'coreutils',
@@ -21,8 +18,6 @@ module.exports = {
     'readline', // ensure gawk gets good readline
     'gawk',
     'gng',
-    // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    'gifsicle',
     'gnupg',
     // Install GNU `sed`, overwriting the built-in `sed`
     // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
@@ -40,7 +35,7 @@ module.exports = {
     'kubernetes',
     // Mac App Store CLI: https://github.com/mas-cli/mas
     'mas',
-    'maven@3.5',
+    // 'maven@3.5',
     // Install some other useful utilities like `sponge`
     'moreutils',
     'nmap',
@@ -68,23 +63,21 @@ module.exports = {
   cask: [
     'caffeine',
     'docker', // docker for mac
-    'google-cloud-sdk',
-    'graalvm/tap/graalvm-ce-java17',
+    // 'google-cloud-sdk',
+    // 'graalvm/tap/graalvm-ce-java17',
     'intellij-idea',
     'iterm2',
-    'java8',
-    'minikube',
+    // 'java8',
+    // 'minikube',
     'scroll-reverser',
     'little-snitch',
     'micro-snitch',
     'sizeup',
-    //'sketchup',
-    'slack',
+    // 'sketchup',
+    // 'slack',
     'spotify',
-    'steam',
     'the-unarchiver',
-    'visual-studio-code',
-    'virtualbox',
+    // 'visual-studio-code',
     'xquartz'
   ],
   gem: [
@@ -93,13 +86,13 @@ module.exports = {
     'artillery',
     'cloc',
     'eslint',
-    'firebase-tools',
+    // 'firebase-tools',
     'gulp',
     'instant-markdown-d',
     'npm-check-updates',
     'prettyjson',
     'trash',
-    'vue',
+    // 'vue',
     'vtop'
   ],
   mas: [
